d65c98d89a7552d869ebc8117a07208f22577f62
takumiw <w.tak.1229@gmail.com>
2020-04-21 14:06:11 +0900

    Feat: generate ecdf features using statsmodels library

--- scripts/generate_ecdf_features.py
+++ scripts/generate_ecdf_features.py
@@ -4,9 +4,10 @@ from datetime import datetime
 import numpy as np
 import pandas as pd
 import glob
-from ecdf import ECDF
-# from statsmodels.distributions.empirical_distribution import ECDF
+from decimal import Decimal, ROUND_HALF_UP, ROUND_HALF_EVEN
+from statsmodels.distributions.empirical_distribution import ECDF
 from obtain_features import ObtainFeatures
+ATC_IDS = [1,2,3,4,5,6]  # Target activity ids
 
 
 # Create log file
@@ -34,78 +35,45 @@ def create_features(acc_raw, gyro_raw):
         tBodyAccXYZ.append(body_acc)
         tGravityAccXYZ.append(grav_acc)
 
-    features = []
+    features = np.empty((0, 60))  # Create 10 ecdf feature values for each triaxial acceleration and gyro signals.
+    idx = np.linspace(0, tBodyAccXYZ[0].shape[0]-1, 10)  # Take 10 linspace percentile.
+    idx = [int(Decimal(str(ix)).quantize(Decimal('0'), rounding=ROUND_HALF_UP)) for ix in idx]
     
-    # print('tBodyAccXYZ', len(tBodyAccXYZ))
     for i in range(len(tBodyAccXYZ)):
-        # print(tBodyAccXYZ[i]['x'].values.reshape(-1, 1).shape)
+        feature_vector = np.array([])
+        for axis in ['x', 'y', 'z']:
+            ecdf = ECDF(tBodyAccXYZ[i][axis].values)  # fit
+            x = ecdf.x[1:]  # Remove -inf
+            feat = x[idx]
+            feature_vector = np.hstack([feature_vector, feat])
 
-        e = ECDF()
-        accxyz = [tBodyAccXYZ[i]['x'].values, tBodyAccXYZ[i]['y'].values, tBodyAccXYZ[i]['z'].values]
-        xyz_acc_features = e._do(accxyz)
-        xyz_acc_features = xyz_acc_features.flatten()
-
-        gyroxyz = [tBodyGyroXYZ[i]['x'].values, tBodyGyroXYZ[i]['y'].values, tBodyGyroXYZ[i]['z'].values]
-        xyz_gyro_features = e._do(gyroxyz)
-        xyz_gyro_features = xyz_gyro_features.flatten()
-
-        feature_vector = np.hstack((xyz_acc_features, xyz_gyro_features))
-        """
-        ecdf = ECDF(tBodyAccXYZ[i]['x'].values)  # fit
-        xnew = np.linspace(ecdf.x[1], ecdf.x[-2], 20)
-        x_acc_features = ecdf(xnew)
-
-        ecdf = ECDF(tBodyAccXYZ[i]['y'].values)  # fit
-        xnew = np.linspace(ecdf.x[1], ecdf.x[-2], 20)
-        y_acc_features = ecdf(xnew)
-
-        ecdf = ECDF(tBodyAccXYZ[i]['z'].values)  # fit
-        xnew = np.linspace(ecdf.x[1], ecdf.x[-2], 20)
-        z_acc_features = ecdf(xnew)
-
-        ecdf = ECDF(tBodyGyroXYZ[i]['x'].values)  # fit
-        xnew = np.linspace(ecdf.x[1], ecdf.x[-2], 20)
-        x_gyro_features = ecdf(xnew)
-
-        ecdf = ECDF(tBodyGyroXYZ[i]['y'].values)  # fit
-        xnew = np.linspace(ecdf.x[1], ecdf.x[-2], 20)
-        y_gyro_features = ecdf(xnew)
-        
-        ecdf = ECDF(tBodyGyroXYZ[i]['z'].values)  # fit
-        xnew = np.linspace(ecdf.x[1], ecdf.x[-2], 20)
-        z_gyro_features = ecdf(xnew)
-        """
-        # feature_vector = np.hstack((x_acc_features, y_acc_features, z_acc_features, x_gyro_features, y_gyro_features, z_gyro_features))
-        features.append(feature_vector)
+        for axis in ['x', 'y', 'z']:
+            ecdf = ECDF(tBodyGyroXYZ[i][axis].values)  # fit
+            x = ecdf.x[1:]  # Remove -inf
+            feat = x[idx]
+            feature_vector = np.hstack([feature_vector, feat])
+        features = np.vstack([features, feature_vector])
     
-    features = np.array(features)
     return features
 
-def create_features_all():
-    logging.debug('Start creating ECDF features')
-    
+
+def main():
+    logging.debug('Start creating ECDF features...')
     train_subjects = [1, 3, 5, 6, 7, 8, 11, 14, 15, 16, 17, 19, 21, 22, 23, 25, 26, 27, 28, 29, 30]
     test_subjects = [2, 4, 9, 10, 12, 13, 18, 20, 24]
     
     root = '../data/hapt_data_set/RawData/'
     acc_files = sorted(glob.glob(root + 'acc*.txt'))
     gyro_files = sorted(glob.glob(root + 'gyro*.txt'))
-    
     label_info = pd.read_table(root + 'labels.txt', sep=' ', header=None, names=['ExpID', 'UserID', 'ActID', 'ActStart', 'ActEnd'])
     
-    X_train = np.array([])
-    X_test = np.array([])
-    # y_train = np.array([])
-    # y_test = np.array([])
+    X_train, X_test = np.empty((0, 60)), np.empty((0, 60))
     
-    # for acc_file, gyro_file in zip(acc_files, gyro_files):
     for acc_file, gyro_file in zip(acc_files, gyro_files):
         exp_id = int(acc_file.split('exp')[1][:2])
         user_id = int(acc_file.split('user')[1][:2])
-        
-        print(f'User ID: {user_id}')
-        
-        temp_label_info = label_info[(label_info.ExpID==exp_id)&(label_info.UserID==user_id)&(label_info.ActID.isin([1,2,3,4,5,6]))]
+        logging.debug(f'Exp ID: {exp_id},  User ID: {user_id}')
+        temp_label_info = label_info[(label_info.ExpID==exp_id)&(label_info.UserID==user_id)&(label_info.ActID.isin(ATC_IDS))]
     
         acc_raw = pd.read_table(acc_file, sep=' ', header=None, names=['x', 'y', 'z'])
         gyro_raw = pd.read_table(gyro_file, sep=' ', header=None, names=['x', 'y', 'z'])
@@ -114,39 +82,20 @@ def create_features_all():
             temp_acc_raw = acc_raw.iloc[act_start:act_end+1]
             temp_gyro_raw = gyro_raw.iloc[act_start:act_end+1]
             features = create_features(temp_acc_raw, temp_gyro_raw)
-            """
-            
-            # labels = [act_id] * len(features)
-            """
             if user_id in train_subjects:
-                if len(X_train) == 0:
-                    X_train = features
-                else:
-                    X_train = np.vstack((X_train, features))
-                """if len(y_train) == 0:
-                    y_train = labels
-                else:
-                    y_train = np.hstack((y_train, labels))"""
+                X_train = np.vstack((X_train, features))
             else:
-                if len(X_test) == 0:
-                    X_test = features
-                else:
-                    X_test = np.vstack((X_test, features))
-                """if len(y_test) == 0:
-                    y_test = labels
-                else:
-                    y_test = np.hstack((y_test, labels))"""
+                X_test = np.vstack((X_test, features))
     
-    logging.debug(f'X_train {X_train.shape}')
-    logging.debug(f'X_test {X_test.shape}')
-    # logging.debug(f'y_train {y_train.shape}')
-    # logging.debug(f'y_test {y_test.shape}')
+    columns = [f'tBody{sensor}ECDF-{axis}{i}' for sensor in ['Acc', 'Gyro'] for axis in ['X', 'Y', 'Z'] for i in range(10)]
+    X_train = pd.DataFrame(X_train, columns=columns)
+    X_test = pd.DataFrame(X_test, columns=columns)
+
+    logging.debug(f'X_train: {X_train.shape}, X_test: {X_test.shape}')
     
-    np.savetxt('../data/my_dataset/ECDF_Iwasawa_X_train.txt', X_train)
-    np.savetxt('../data/my_dataset/ECDF_Iwasawa_X_test.txt', X_test)
-    # np.savetxt('../data/my_dataset/ECDF_y_train.txt', y_train)
-    # np.savetxt('../data/my_dataset/y_test.txt', y_test)
+    pd.to_pickle('../data/my_dataset/ECDF_X_train.pickle', X_train)
+    pd.to_pickle('../data/my_dataset/ECDF_X_test.pickle', X_test)
 
 
 if __name__ == '__main__':
-    create_features_all()
+    main()
