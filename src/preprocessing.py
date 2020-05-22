171ad034cfbc69af6ff8b2a104de06d721646356
takumiw <w.tak.1229@gmail.com>
2020-05-23 01:36:38 +0900

    Fix: bug in src/preprocessing.py

--- src/preprocessing.py
+++ src/preprocessing.py
@@ -1,180 +1,182 @@
 # -*- coding:utf-8 -*-
+import logging
 import numpy as np
 from numpy.linalg import norm
 import pandas as pd
 import math
 from decimal import Decimal, ROUND_HALF_UP, ROUND_HALF_EVEN
-from scipy import signal, stats
+from scipy import stats
+from scipy.signal import butter, filtfilt
+from scipy.fftpack import fft
 from statsmodels.regression.linear_model import burg
 from statsmodels.distributions.empirical_distribution import ECDF
 
 
-class ObtainFeatures():
-    fs = 50  # Sampling frequency of sensor data.
-    win_size = 128  # WIndow size of sliding window to segment raw signals.
-    overlap_rate = 0.5  # Overlap rate of sliding window to segment raw signals.
-    win_sec = 2.56  # Elapsing time (second) of window. 
+class Preprocess():    
     
-    
-    def __init__(self, acc, gyro):
+    def __init__(self, fs=50):
         """
         Args:
-            acc (DataFrame): Accelerometer 3-axial raw signals
-            gyro (DataFrame): Gyroscope 3-axial raw signals
+            fs (int, default=50): Sampling frequency of sensor signals
         """
-        self.acc = acc
-        self.gyro = gyro
-        self.acc_seg = None
-        self.gyro_seg = None
+        self.fs = fs
         
-        
-    def apply_median_filter(self, sensor_signal):
+    
+    def apply_filter(self, signal, filter='median', window=5):
         """
-        A median filter with window length 5 is applied to remove noise in signals.
+        A denosing filter is applied to remove noise in signals.
         Args:
-            sensor_signal (DataFrame): Raw signal
+            signal (pandas.DataFrame): Raw signal
+            filter (str, default:'median'): Filter name is any of 'mean', 'median', or 'butterworth'
+            window (int, default:5): Length of filter
         Returns:
-            retval (DataFrame): Median filtered signal
+            signal (pandas.DataFrame): Filtered signal
+        See Also:
+            'butterworth' applies a 3rd order low-pass Butterworth filter with a corner frequency of 20 Hz.
         """
-        return sensor_signal.rolling(window=5, center=True, min_periods=1).median()
+        if filter == 'mean':
+            signal = signal.rolling(window=window, center=True, min_periods=1).mean()
+        elif filter == 'median':
+            signal = signal.rolling(window=window, center=True, min_periods=1).median()
+        elif filter == 'butterworth':
+            fc = 20  # cutoff frequency
+            w = fc / (self.fs / 2)  # Normalize the frequency
+            b, a = butter(3, w, 'low')  # 3rd order low-pass Butterworth filter
+            signal = pd.DataFrame(filtfilt(b, a, signal, axis=0), columns=signal.columns)
+        else:
+            try:
+                raise ValueError('Not defined filter. See Args.')
+            except ValueError as e:
+                logging.error(traceback.format_exc())
         
-        
-    def apply_butterworth_filter(self, sensor_signal):
-        """
-        A 3rd order low pass Butterworth filter with a corner frequency of 20 Hz is applied to remove noise in signals.
-        Args:
-            sensor_signal (DataFrame): Raw signal
-        Returns:
-            retval (DataFrame): Butterworth filtered signal
-        """       
-        fc = 20  # cutoff frequency
-        w = fc / (self.__class__.fs / 2)  # Normalize the frequency
-        b, a = signal.butter(3, w, 'low')  # 3rd order low pass Butterworth filter
-        
-        return pd.DataFrame(signal.filtfilt(b, a, sensor_signal, axis=0), columns=['x', 'y', 'z'])  # Apply Butterworth filter
-       
+        return signal
     
-    def segment_signal(self, acc, gyro):
+
+    def normalize(self, signal):
+        """
+        Apply normalization
+        Args:
+            signal (pandas.DataFrame): Raw signal
+        Returns:
+            signal (pandas.DataFrame): Normalized signal
+        """
+        df_mean = signal.mean()
+        df_std = signal.std()
+        signal = (signal - df_mean) / df_std
+        return signal
+    
+    
+    def segment_signal(self, signal, window_size=128, overlap_rate=0.5, res_type='dataframe'):
         """
         Sample sensor signals in fixed-width sliding windows of 2.56 sec and 50% overlap (128 readings/window).
         Args:
-            acc (DataFrame): Raw acceleration signal
-            gyro (DataFrame): Raw gyro signal
+            signal (pandas.DataFrame): Raw signal
+            window_size (int, default=128): Window size of sliding window to segment raw signals.
+            overlap_rate (float, default=0.5): Overlap rate of sliding window to segment raw signals.
+            res_type (str, default='dataframe'): Type of return value; 'array' or 'dataframe'
         Returns:
-            acc_seg (list): List of segmented acceleration sigmal. Element is a segmented DataFrame.
-            gyro_seg (list): List of segmented gyro sigmal. Element is a segmented DataFrame.
-        """
-        assert len(acc) == len(gyro), f'Length of acceleration signal ({len(acc)}) does not match to length of gyro signal ({len(gyro)}).'
+            signal_seg (list of pandas.DataFrame): List of segmented sigmal.
+        """        
+        signal_seg = []
         
-        win_size = self.__class__.win_size
-        overlap_rate = self.__class__.overlap_rate
-        acc_seg, gyro_seg = [], []
+        for start_idx in range(0, len(signal) - window_size, int(window_size * overlap_rate)):
+            seg = signal.iloc[start_idx:start_idx + window_size].reset_index(drop=True)
+            if res_type == 'array':
+                seg = seg.values
+            signal_seg.append(seg)
         
-        for start_idx in range(0, len(acc) - win_size, int(win_size * overlap_rate)):
-            acc_seg.append(acc.iloc[start_idx:start_idx + win_size].reset_index(drop=True))
-            gyro_seg.append(gyro.iloc[start_idx:start_idx + win_size].reset_index(drop=True))
+        if res_type == 'array':
+            signal_seg = np.array(signal_seg)
 
-        return acc_seg, gyro_seg
+        return signal_seg
         
     
-    def remove_gravity(self, acc):
+    def separate_gravity(self, acc):
         """
         Separate acceleration signal into body and gravity acceleration signal.
         Another low pass Butterworth filter with a corner frequency of 0.3 Hz is applied.
         Args:
-            acc (DataFrame): Segmented acceleration signal
+            acc (pandas.DataFrame): Segmented acceleration signal
         Returns:
-            acc_body (DataFrame): Body acceleration signal
-            acc_grav (DataFrame): Gravity acceleration signal
+            acc_body (pandas.DataFrame): Body acceleration signal
+            acc_grav (pandas.DataFrame): Gravity acceleration signal
         """
         fc = 0.3  # cutoff frequency
-        w = fc / (self.__class__.fs / 2)  # Normalize the frequency
-        b, a = signal.butter(3, w, 'low')  # 3rd order low pass Butterworth filter
-        
-        acc_grav = pd.DataFrame(signal.filtfilt(b, a, acc, axis=0), columns=['x', 'y', 'z'])  # Apply Butterworth filter
+        w = fc / (self.fs / 2)  # Normalize the frequency
+        b, a = butter(3, w, 'low')  # 3rd order low pass Butterworth filter
+        acc_grav = pd.DataFrame(filtfilt(b, a, acc, axis=0), columns=acc.columns)  # Apply Butterworth filter
         
         # Substract gravity acceleration from acceleration sigal.
         acc_body = acc - acc_grav
         return acc_body, acc_grav
     
     
-    def obtain_jerk_signal(self, sensor_signal):
+    def obtain_jerk_signal(self, signal):
         """
         Derive signal to obtain Jerk signals
         Args:
-            sensor_signal (DataFrame)
+            signal (pandas.DataFrame)
         Returns:
-            jerk_signal (DataFrame):
+            jerk_signal (pandas.DataFrame):
         """
-        jerk_signal = sensor_signal.diff(periods=1)  # Calculate difference 
+        jerk_signal = signal.diff(periods=1)  # Calculate difference 
         jerk_signal.iloc[0] = jerk_signal.iloc[1]  # Fillna
-        jerk_signal = jerk_signal / (1 / self.__class__.fs)  # Derive in time (1 / sampling frequency)
+        jerk_signal = jerk_signal / (1 / self.fs)  # Derive in time (1 / sampling frequency)
         return jerk_signal
     
     
-    def obtain_magnitude(self, sensor_signal):
+    def obtain_magnitude(self, signal):
         """
         Calculate the magnitude of these three-dimensional signals using the Euclidean norm
         Args:
-            sensor_signal (DataFrame): Three-dimensional signals
+            signal (pandas.DataFrame): Three-dimensional signals
         Returns:
-            retval (array): Magnitude of three-dimensional signals
+            res (pandas.DataFrame): Magnitude of three-dimensional signals
         """
-        return  norm(sensor_signal, ord=2, axis=1)
+        return pd.DataFrame(norm(signal, ord=2, axis=1))
     
     
-    def obtain_amplitude_spectrum(self, sensor_signal):
+    def obtain_spectrum(self, signal):
         """
-        Obtain amplitude spectrum using Fast Fourier Transform (FFT).
+        Obtain spectrum using Fast Fourier Transform (FFT).
         Args:
-            sensor_signal (DataFrame): Time domain signals
+            signal (pandas.DataFrame): Time domain signals
         Returns:
-            amp (array): Amplitude spectrum
+            amp (pandas.DataFrame): Amplitude spectrum
+            phase (pandas.DataFrame): Phase spectrum
         """
-        N = len(sensor_signal)
-        # dim = sensor_signal.shape[1]
-        win = np.hamming(N)  # hamming window
+        N = len(signal)
+        columns = signal.columns
+
+        for col in columns:
+            signal[col] = signal[col] * np.hamming(N)  # hamming window
         
-        if len(sensor_signal.shape) == 1:
-            sensor_signal = sensor_signal * win  # Apply hamming window
-            F = np.fft.fft(sensor_signal, axis=0)  # Apply FFT
-            F = F[:N//2]  # Remove the overlapping part
+        F = fft(signal, axis=0)  # Apply FFT
+        F = F[:N//2, :]  # Remove the overlapping part
 
-            amp = np.abs(F)  # Obtain the amplitude spectrum
-            amp = amp / N * 2
-            amp[0] = amp[0] / 2
-        
-        else:
-            df = pd.DataFrame(columns=['x', 'y', 'z'])
-            for axis in ['x', 'y', 'z']:
-                df[axis] = sensor_signal[axis] * win  # Apply hamming window
+        amp = np.abs(F)  # Obtain the amplitude spectrum
+        amp = amp / N * 2
+        amp[0] = amp[0] / 2
+        amp = pd.DataFrame(amp, columns=columns)  # Convert array to DataFrame
+        phase = np.angle(F)
+        phase = pd.DataFrame(phase, columns=columns)  # Convert array to DataFrame
 
-            F = np.fft.fft(df, axis=0)  # Apply FFT
-            F = pd.DataFrame(F, columns=['x', 'y', 'z'])  # Convert array to DataFrame
-            F = F[:N//2]  # Remove the overlapping part
-
-            amp = np.abs(F)  # Obtain the amplitude spectrum
-            amp = pd.DataFrame(amp, columns=['x', 'y', 'z'])  # Convert array to DataFrame
-
-            amp = amp / N * 2
-            amp.iloc[0] = amp.iloc[0] / 2
-        
-        return amp
+        return amp, phase
     
 
-    def obtain_ecdf_percentile(self, sensor_signal, n_bins=10):
+    def obtain_ecdf_percentile(self, signal, n_bins=10):
         """Obtain ECDF (empirical cumulative distribution function) percentile values.
         Args:
-            sensor_signal (DataFrame): Time domain signals
+            signal (DataFrame): Time domain signals
             n_bins (int, default: 10): How many percentiles to use as a feature
         Returns:
             features (array): ECDF percentile values.
         """
-        idx = np.linspace(0, sensor_signal.shape[0]-1, n_bins)  # Take n_bins linspace percentile.
+        idx = np.linspace(0, signal.shape[0]-1, n_bins)  # Take n_bins linspace percentile.
         idx = [int(Decimal(str(ix)).quantize(Decimal('0'), rounding=ROUND_HALF_UP)) for ix in idx]
         features = np.array([])
-        for axis in ['x', 'y', 'z']:
-            ecdf = ECDF(sensor_signal[axis].values)  # fit
+        for col in signal.columns:
+            ecdf = ECDF(signal[col].values)  # fit
             x = ecdf.x[1:]  # Remove -inf
             feat = x[idx]
             features = np.hstack([features, feat])
@@ -182,91 +184,73 @@ class ObtainFeatures():
         return features
     
     
-    def obtain_mean(self, sensor_signal) -> np.ndarray:
-        if len(sensor_signal.shape) == 1:    return sensor_signal.mean()
-        else:    return sensor_signal.mean().values
+    def obtain_mean(self, signal) -> np.ndarray:
+        return signal.mean().values
     
-    def obtain_std(self, sensor_signal) -> np.ndarray:
-        if len(sensor_signal.shape) == 1:    return sensor_signal.std()
-        else:    return sensor_signal.std().values
+    def obtain_std(self, signal) -> np.ndarray:
+        return signal.std().values
     
-    def obtain_mad(self, sensor_signal) -> np.ndarray:
-        return stats.median_absolute_deviation(sensor_signal, axis=0)
+    def obtain_mad(self, signal) -> np.ndarray:
+        return stats.median_absolute_deviation(signal, axis=0)
     
-    def obtain_max(self, sensor_signal) -> np.ndarray:
-        if len(sensor_signal.shape) == 1:    return sensor_signal.max()
-        else:    return sensor_signal.max().values
+    def obtain_max(self, signal) -> np.ndarray:
+        return signal.max().values
     
-    def obtain_min(self, sensor_signal) -> np.ndarray:
-        if len(sensor_signal.shape) == 1:    return sensor_signal.min()
-        else:    return sensor_signal.min().values
+    def obtain_min(self, signal) -> np.ndarray:
+        return signal.min().values
     
-    def obtain_sma(self, sensor_signal) -> np.ndarray:
-        if len(sensor_signal.shape) == 1:    return (sensor_signal.sum() - self.obtain_min(sensor_signal) * len(sensor_signal)) / self.__class__.win_sec
-        else:    return sum(sensor_signal.sum().values - self.obtain_min(sensor_signal) * len(sensor_signal)) / self.__class__.win_sec
+    def obtain_sma(self, signal, window_size=128) -> np.ndarray:
+        window_second = window_size / self.fs
+        return sum(signal.sum().values - self.obtain_min(signal) * len(signal)) / window_second
     
-    def obtain_energy(self, sensor_signal) -> np.ndarray:
-        return  norm(sensor_signal, ord=2, axis=0) ** 2 / len(sensor_signal)
+    def obtain_energy(self, signal) -> np.ndarray:
+        return  norm(signal, ord=2, axis=0) ** 2 / len(signal)
     
-    def obtain_iqr(self, sensor_signal) -> np.ndarray:
-        if len(sensor_signal.shape) == 1:
-            q25, q75 = np.percentile(sensor_signal, q=[25, 75])
-            return q75 - q25
-        else:    return  sensor_signal.quantile(.75).values - sensor_signal.quantile(.25).values
+    def obtain_iqr(self, signal) -> np.ndarray:
+        return  signal.quantile(.75).values - signal.quantile(.25).values
     
-    def obtain_entropy(self, sensor_signal) -> np.ndarray:
-        sensor_signal = sensor_signal - sensor_signal.min()
-        return  stats.entropy(sensor_signal)
+    def obtain_entropy(self, signal) -> np.ndarray:
+        signal = signal - signal.min()
+        return  stats.entropy(signal)
 
-    def obtain_arCoeff(self, sensor_signal) -> np.ndarray:
-        if len(sensor_signal.shape) == 1:  # Signal dimension is 1
-            arCoeff, _ = burg(sensor_signal, order=4)
-        else:  # Signal dimension is 3
-            x, _ = burg(sensor_signal['x'], order=4)
-            y, _ = burg(sensor_signal['y'], order=4)
-            z, _ = burg(sensor_signal['z'], order=4)
-            arCoeff = np.hstack((x, y, z))
+    def obtain_arCoeff(self, signal) -> np.ndarray:
+        arCoeff = np.array([])
+        for col in signal.columns:
+            val, _ = burg(signal[col], order=4)
+            arCoeff = np.hstack((arCoeff, val))
         return arCoeff
     
-    def obtain_correlation(self, sensor_signal) -> np.ndarray:
-         if len(sensor_signal.shape) == 1:  # Signal dimension is 1
+    def obtain_correlation(self, signal) -> np.ndarray:
+        if signal.shape[1] == 1:  # Signal dimension is 1
             correlation = np.array([])
-         else:  # Signal dimension is 3
-            xy = np.corrcoef(sensor_signal['x'], sensor_signal['y'])[0][1]
-            yz = np.corrcoef(sensor_signal['y'], sensor_signal['z'])[0][1]
-            zx = np.corrcoef(sensor_signal['z'], sensor_signal['x'])[0][1]
-            correlation = np.hstack((xy, yz, zx))
-         return correlation
-
-    def obtain_maxInds(self, sensor_signal) -> np.ndarray:
-        if len(sensor_signal.shape) == 1:    return np.argmax(sensor_signal)
-        return sensor_signal.idxmax().values
-        
-    def obtain_meanFreq(self, sensor_signal) -> np.ndarray:
-        if len(sensor_signal.shape) == 1:  # Signal dimension is 1
-            meanFreq = np.mean(sensor_signal * np.arange(len(sensor_signal)))
         else:  # Signal dimension is 3
-            x = np.mean(sensor_signal['x'] * sensor_signal.index.values)
-            y = np.mean(sensor_signal['y'] * sensor_signal.index.values)
-            z = np.mean(sensor_signal['z'] * sensor_signal.index.values)
-            meanFreq = np.hstack((x, y, z))
+            xy = np.corrcoef(signal['x'], signal['y'])[0][1]
+            yz = np.corrcoef(signal['y'], signal['z'])[0][1]
+            zx = np.corrcoef(signal['z'], signal['x'])[0][1]
+            correlation = np.hstack((xy, yz, zx))
+        return correlation
+
+    def obtain_maxInds(self, signal) -> np.ndarray:
+        return signal.idxmax().values
+        
+    def obtain_meanFreq(self, signal) -> np.ndarray:
+        meanFreq = np.array([])
+        for col in signal.columns:
+            val = np.mean(signal[col] * np.arange(len(signal)))
+            meanFreq = np.hstack((meanFreq, val))
         return meanFreq
     
-    def obtain_skewness(self, sensor_signal) -> np.ndarray:
-        if len(sensor_signal.shape) == 1:
-            sensor_signal = pd.DataFrame(sensor_signal)
-        return sensor_signal.skew().values
+    def obtain_skewness(self, signal) -> np.ndarray:
+        return signal.skew().values
     
-    def obtain_kurtosis(self, sensor_signal) -> np.ndarray:
-        if len(sensor_signal.shape) == 1:
-            sensor_signal = pd.DataFrame(sensor_signal)
-        return sensor_signal.kurt().values
+    def obtain_kurtosis(self, signal) -> np.ndarray:
+        return signal.kurt().values
     
-    def obtain_bandsEnergy(self, sensor_signal) -> np.ndarray:
+    def obtain_bandsEnergy(self, signal) -> np.ndarray:
         bandsEnergy = np.array([])
         bins =  [0, 4, 8, 12, 16, 20, 24, 29, 34, 39, 44, 49, 54, 59, 64]
         for i in range(len(bins) - 1):
-            df = sensor_signal.iloc[bins[i]:bins[i+1]]
+            df = signal.iloc[bins[i]:bins[i+1]]
             arr = self.obtain_energy(df)
             bandsEnergy = np.hstack((bandsEnergy, arr))
         return bandsEnergy
