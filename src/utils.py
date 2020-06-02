f46c1b54a6314faa29cad2f04f4664a5820fdce7
takumiw <w.tak.1229@gmail.com>
2020-06-02 19:26:15 +0900

    Update: format src/utils.py

--- src/utils.py
+++ src/utils.py
@@ -1,5 +1,6 @@
 # -*- coding:utf-8 -*-
 
+
 def color_generator(i):
-    l = ['#FFAF6D', '#DC4195', '#F1E898', '#6DCBB9', '#3E89C4', '#6F68CF']
+    l = ["#FFAF6D", "#DC4195", "#F1E898", "#6DCBB9", "#3E89C4", "#6F68CF"]
     return l[i]
