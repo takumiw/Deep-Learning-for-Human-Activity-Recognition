91b9d188f9d7b0b1831977a3e09f8836d9bf3289
takumiw <w.tak.1229@gmail.com>
2020-09-18 19:46:33 +0900

    remove comment out

--- run_lgbm.py
+++ run_lgbm.py
@@ -39,84 +39,6 @@ logger = getLogger(__name__)
 logger.setLevel(DEBUG)
 logger.debug(f"{LOG_DIR}/{EXEC_TIME}.log")
 
-# Load selected feature list
-# selected_feature = pd.read_pickle('../configs/selected_feature.pickle')
-
-
-'''def load_data():
-    """Load dataset.
-    The following six classes are included in this experiment.
-        - WALKING, WALKING_UPSTAIRS, WALKING_DOWNSTAIRS, SITTING, STANDING, LAYING
-    The following transition classes are excluded.
-        - STAND_TO_SIT, SIT_TO_STAND, SIT_TO_LIE, LIE_TO_SIT, STAND_TO_LIE, and LIE_TO_STAND
-    Args:
-    Returns:
-        X_train (pandas.DataFrame): Explanatory variable in train data
-        X_test (pandas.DataFrame): Explanatory variable in test data
-        y_train (pandas.DataFrame): Teacher data in train data
-        y_test (pandas.DataFrame): Teacher data in test data
-        subject_id_train (numpy.array): subject_id for each record
-        subject_id_test (numpy.array): subject_id for each record
-        label2activity_dict (dict): key:label_id, value: title_of_class
-        activity2label_dict (dict): key:title_of_class, value: label_id
-    """
-    root = "data/hapt_data_set/"
-    X_train = pd.read_pickle("data/my_dataset/X_train.pickle")
-    y_train = pd.DataFrame(np.load("data/my_dataset/y_train.npy"))
-    subject_id_train = pd.read_table(root + "Train/subject_id_train.txt", sep=" ", header=None)
-
-    X_test = pd.read_pickle("data/my_dataset/X_test.pickle")
-    y_test = pd.DataFrame(np.load("data/my_dataset/y_test.npy"))
-    subject_id_test = pd.read_table(root + "Test/subject_id_test.txt", sep=" ", header=None)
-
-    activity_labels = pd.read_table(root + "activity_labels.txt", header=None).values.flatten()
-    activity_labels = np.array([label.rstrip().split() for label in activity_labels])
-    label2activity_dict, activity2label_dict = {}, {}
-    for label, activity in activity_labels:
-        label2activity_dict[int(label)] = activity
-        activity2label_dict[activity] = int(label)
-
-    class_names_inc = [
-        "WALKING",
-        "WALKING_UPSTAIRS",
-        "WALKING_DOWNSTAIRS",
-        "SITTING",
-        "STANDING",
-        "LAYING",
-    ]
-    class_ids_inc = [activity2label_dict[c] for c in class_names_inc]
-
-    idx_train = y_train[y_train[0].isin(class_ids_inc)].index
-    X_train = X_train.iloc[idx_train].reset_index(drop=True)
-    y_train = y_train.iloc[idx_train].reset_index(drop=True)
-    subject_id_train = subject_id_train.iloc[idx_train].reset_index(drop=True)
-
-    idx_test = y_test[y_test[0].isin(class_ids_inc)].index
-    X_test = X_test.iloc[idx_test].reset_index(drop=True)
-    y_test = y_test.iloc[idx_test].reset_index(drop=True)
-    subject_id_test = subject_id_test.iloc[idx_test].reset_index(drop=True)
-
-    # Replace 6 to 0
-    rep_activity = label2activity_dict[6]
-    label2activity_dict[0] = rep_activity
-    label2activity_dict.pop(6)
-    activity2label_dict[rep_activity] = 0
-
-    y_train = y_train.replace(6, 0)
-    y_test = y_test.replace(6, 0)
-
-    return (
-        X_train,
-        X_test,
-        y_train,
-        y_test,
-        subject_id_train,
-        subject_id_test,
-        label2activity_dict,
-        activity2label_dict,
-    )'''
-
-
 (
     X_train,
     X_test,
@@ -127,9 +49,6 @@ logger.debug(f"{LOG_DIR}/{EXEC_TIME}.log")
     label2act,
     act2label,
 ) = load_features()
-# ) = load_data()
-# X_train = X_train[selected_feature]
-# X_test = X_test[selected_feature]
 logger.debug(f"{X_train.shape=} {X_test.shape=}")
 logger.debug(f"{y_train.shape=} {y_test.shape=}")
 
